38d3ec236176dd43a46aa074a9278778fe96199d
Yash-Sheliya-Apexture <yash@apexture.in>
2025-06-13 10:09:37 +0530

    add new code

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,3 +1,10 @@
+// export default {
+//     mongoUrl: process.env.MONGO_URI || 'mongodb+srv://yashmorenainfotech6:vDl2PO8fAYoVCd6A@cluster0.ibpvm.mongodb.net/wise?retryWrites=true&w=majority&appName=Cluster0', // Replace with your MongoDB URI or connection string - IMPORTANT!
+// };
+
+// backend/config/database.config.js
+
 export default {
-    mongoUrl: process.env.MONGO_URI || 'mongodb+srv://yashmorenainfotech6:vDl2PO8fAYoVCd6A@cluster0.ibpvm.mongodb.net/wise?retryWrites=true&w=majority&appName=Cluster0', // Replace with your MongoDB URI or connection string - IMPORTANT!
+    // This forces the app to use the MONGO_URI from your environment variables.
+    mongoUrl: process.env.MONGO_URI, 
 };
